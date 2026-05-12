35acea5c480c2e4c1a2cfef871230ad880d9c386
Kanabig <parkjungho20@gmail.com>
2026-05-12 17:26:55 +0900

    260512 task complete

---
+++ 260512ex/ex01/list.py
@@ -0,0 +1,70 @@
+# animals = ['개똥지빠귀', '붉은꼬리매', '붉은머리오목눈이', '흰머리수리', '산비둘기', '쏙독새', '뻐꾸기', '올빼미']
+# complexList = [10, 3.14, 'a', 'hello']
+# attendList = ['이순철', '김병헌', '김민우', '박찬호', '김민태']
+
+# print(type(animals))
+# print(type(complexList))
+# print(type(attendList))
+# print(type(animals) is type(complexList))
+
+# print(animals[len(animals) - 1])
+# print(animals[-1])
+   
+
+# -----------------------------------------------------------------
+
+
+# input_str = input('메시지 입력: ')
+# print(f'input_str({len(input_str)}): {input_str}')
+
+
+# -----------------------------------------------------------------
+
+
+# animals = ['개똥지빠귀', '붉은꼬리매', '붉은머리오목눈이', '흰머리수리', '쏙독새']
+# for idx, item in enumerate(animals):
+#     print(item, idx)
+
+
+# -----------------------------------------------------------------
+
+
+# hobbies = []
+
+# while True:
+#     str = input('취미를 입력하세요(엔터 시 종료):')
+#     if str == '': break
+#     hobbies.append(str)
+
+# print(f'hobbies({len(hobbies)}): {hobbies}')
+
+
+# -----------------------------------------------------------------
+
+
+# animals = ['개똥지빠귀', '붉은꼬리매', '붉은머리오목눈이', '흰머리수리', '쏙독새', '흰머리수리']
+# animals.insert(1, '뻐꾸기')
+
+# print(animals)
+
+# list = [1, 2, 3, 4, 5, 7, 8, 9]
+# print(list)
+
+# list.insert(list.index(7), 6)
+# list.append(10)
+# list.insert(len(list), 10)
+# print(list)
+
+
+# -----------------------------------------------------------------
+
+
+# animals = ['개똥지빠귀', '붉은꼬리매', '붉은머리오목눈이', '흰머리수리', '쏙독새']
+# # lefted = animals.pop(1)
+# # del animals[1]
+# animals.remove('붉은머리오목눈이')
+# print(animals)
+
+
+# -----------------------------------------------------------------
+
---
+++ 260512ex/ex01/task.py
@@ -0,0 +1,7 @@
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    content = f.read()
+    inputs = content.split()
+    print(inputs)
+
